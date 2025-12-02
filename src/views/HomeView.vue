90726c3162b50b7775652a1f99d1484b39ae2ff8
FireOnIcecube <fireonicecube@gmail.com>
2025-12-02 21:10:11 +0800

    chore: 注释掉部分代码以进行重构
    将 App.vue 和 HomeView.vue 中的 header 和 main 标签注释掉，并添加新的 main 标签以展示网站建设中的信息。同时，注释掉 main 样式中的 #app 选择器，以便进行样式重构。

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,19 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
+  <!-- <main>
     <TheWelcome />
+  </main> -->
+
+  <main
+    style="
+      display: grid;
+      place-items: center; /* 水平 + 垂直置中 */
+      height: 100vh; /* 佔滿整個視窗高度 */
+    "
+  >
+    <h1>
+      DAP-HAI Research Center Advanced Human–AI Co-Adaptive Frameworks Website Under Construction
+    </h1>
   </main>
 </template>
