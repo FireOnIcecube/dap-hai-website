5dc4d05f7878d4cfa01fd010ce079576660ed2c6
FireOnIcecube <fireonicecube@gmail.com>
2025-12-09 10:23:19 +0800

    chore: 新增 i18n-vue 以及 tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
-import './assets/main.css'
+// import './assets/main.css'
+import './assets/styles.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
 
 import App from './App.vue'
 import router from './router'
 
+const i18n = createI18n({})
+
 const app = createApp(App)
 
+app.use(i18n)
 app.use(createPinia())
 app.use(router)
 
